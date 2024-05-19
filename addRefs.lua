948efa240bcaecdb5db6635220ff679e1fa28ddc
johnd0e <1838643+johnd0e@users.noreply.github.com>
2024-05-20 01:28:34 +0200

    hlf: filter to make index and insert refs-block in every article

---
+++ addRefs.lua
@@ -0,0 +1,51 @@
+﻿-- For hlf:
+-- Make index page (out of pandoc-generated toc)
+-- Add refs-block ("other help sections") to each article.
+
+-- requires: meta.refsTemplate and --toc
+-- optional: meta.refsLevel
+
+--luacheck: globals Refs meta 
+local function Pandoc (doc)
+  meta = doc.meta
+  if not meta.refsTemplate then
+    --print("addRefs: no refsTemplate, exiting")
+    return
+  end
+  local insertRefs = require(meta.refsTemplate[1].text)
+  if not insertRefs then
+    return
+  end
+  -- add Index section
+  local toc = pandoc.structure.table_of_contents(doc.blocks):walk{Link=function(el)
+    el.attr = pandoc.Attr() -- remove auto-id's "toc-title"
+    return el
+  end}
+  doc.blocks:insert(pandoc.Header(1, "Index", pandoc.Attr("index", nil, {refsTitle="i"})))
+  doc.blocks:insert(toc)
+  --
+  Refs = pandoc.List{} -- list of linked articles
+  local refsLevel = meta.refsLevel
+  refsLevel = not refsLevel and 1 or refsLevel[1] and tonumber(refsLevel[1].text)
+  doc.blocks:walk { -- for refs pick all Headers with level<=meta.refsLevel (default 1)
+    Header=function(el)
+      if el.identifier~="" and not el.classes:includes("noref") then
+        if refsLevel and refsLevel<el.level then return end
+        local content, title = el.content
+        if el.attributes.refsTitle then
+          title = pandoc.utils.stringify(content)
+          content = el.attributes.refsTitle
+        end
+        Refs:insert(pandoc.Link(content, el.identifier, title))
+      end
+    end
+  }
+  --
+  require"sections"(doc.blocks, insertRefs)
+  return doc
+end
+
+return {
+  { Pandoc=Pandoc },
+  { Div=function(el) return el.content end }, -- HtmlToFarHelp does not support Divs
+}
---
+++ refsBottom.lua
@@ -0,0 +1,45 @@
+﻿--luacheck: globals meta Refs
+local Label = meta.refsLabel
+if not Label then -- default template of the links block
+  local doc = pandoc.read[[
+---
+refsLabel: >
+
+  - - -
+
+  Другие разделы справки:\ 
+...
+]]
+  Label = doc.meta.refsLabel
+end
+
+local function prepRefs (header) -- prepare refs section for current article
+  local refs = Label:walk{} -- copy
+  local inlines = refs[#refs].content
+  --
+  local links = Refs:filter(function (el) -- remove self from refs
+    return el.target~=header.identifier
+  end)
+  local last = links[#links]
+  for _,el in ipairs(links) do
+    inlines:extend{pandoc.Str"[", el, pandoc.Str"]"}
+    if el~=last then inlines:extend{pandoc.Str",", pandoc.Space()} end
+  end
+  return pandoc.Div(refs)
+end
+
+local function insertRefs (blocks, startIdx, endIdx)
+  local header = blocks[startIdx]
+  if header.identifier=="index" -- skip Index section
+      or header.classes:includes("noref") then --e.g. cfgscript note
+    return
+  end
+  local i = endIdx
+  if blocks[i-1].tag=="Div" then -- insert ref block before div
+    i = i-1
+  end
+  blocks:insert(i, prepRefs(header))
+  return endIdx+1
+end
+
+return insertRefs
