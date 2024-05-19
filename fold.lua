cfdc2dd20db249e14b164865745616b2e85d54af
johnd0e <1838643+johnd0e@users.noreply.github.com>
2024-05-19 20:22:51 +0200

    Refactor fold.lua

--- fold.lua
+++ fold.lua
@@ -1,15 +1,12 @@
 -- Save space wrapping all (except first) sections content inside spoilers
 
-local function condition (header, blocks, idx)
-  if pandoc.utils.stringify(header):match"cfgscript" then --hardcoded for now
-    blocks[idx] = pandoc.HorizontalRule()
-    return "skip"
-  end
-end
-
 local function wrapSection (blocks, startIdx, endIdx)
   -- transform headers into plain (but formatted) text
-  blocks[startIdx] = pandoc.Para{pandoc.RawInline("markdown", "[u]"), pandoc.Strong(blocks[startIdx].content), pandoc.RawInline("markdown","[/u]")}
+  blocks[startIdx] = pandoc.Para {
+    pandoc.RawInline("markdown", "[u]"),
+    pandoc.Strong(blocks[startIdx].content),
+    pandoc.RawInline("markdown","[/u]")
+  }
   blocks[startIdx].content:insert(pandoc.RawInline("markdown", " [spoiler]"))
   blocks:insert(endIdx, pandoc.RawBlock("markdown", "[/spoiler]"))
   return endIdx+1
@@ -17,16 +14,19 @@ end
 
 local first = true
 return {
-  { -- remove redundant first header, as forum topic already has own title
-    Header=function()
-      if first then
+  {
+    Header=function(el)
+      if first then -- remove redundant first header, as forum topic already has own title
         first = false
         return {}
+      end
+      if el.classes:includes("noref") then --e.g. cfgscript note
+        return pandoc.HorizontalRule()
       end
     end
   }, { -- add spoilers
     Pandoc=function(doc)
-      require"sections"(doc.blocks, wrapSection, condition)
+      require"sections"(doc.blocks, wrapSection)
       return doc
     end
   }
