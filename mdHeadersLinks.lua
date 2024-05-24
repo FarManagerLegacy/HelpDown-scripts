50a8c27aa797a7306f0c8d05b7c39e37fc83f419
johnd0e <1838643+johnd0e@users.noreply.github.com>
2024-05-25 01:03:13 +0200

    mdHeadersLinks.lua: more reliable id's, relying on pandoc internal processing

--- mdHeadersLinks.lua
+++ mdHeadersLinks.lua
@@ -2,29 +2,20 @@
 -- we need to rewrite links to use implicit headers id's
 -- https://github.com/jgm/pandoc/discussions/9794
 
--- unable to use gsub("%p" because string functions are not utf8-aware
-local p = ".,:;?!/\\|<>()[]{}^°\"'#~´`=+*@$§&"
-p = "["..p.."]"
-local ids = {}
-local function make_id (el)
-  local title = pandoc.utils.stringify(el)
-  local id = title:gsub(p,""):gsub(" ", "-")
-  if ids[id] then
-    local i,candidate = 0
-    repeat
-      i = i+1
-      candidate = id.."-"..i
-    until not ids[candidate]
-    id = candidate
-  end
-  ids[id] = true
-  return id
+-- adapted from https://stackoverflow.com/a/72600888/2520247
+local temp_doc = pandoc.Pandoc{}
+local function make_id (el, via)
+  local via = via or "html"
+  table.insert(temp_doc.blocks, pandoc.Header(1, el.content))
+  local roundtripped_doc = pandoc.read(pandoc.write(temp_doc, via), via)
+  local blocks = roundtripped_doc.blocks
+  return blocks[#blocks].identifier
 end
 
 local headers = {}
 local function Header (el)
   if el.identifier=="" then return end
-  local id = make_id(el)
+  local id = make_id(el, "gfm")
   if id~=el.identifier then
     headers["#"..el.identifier] = "#"..id
   end
