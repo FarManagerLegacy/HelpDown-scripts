2d671f41f320be2e7cdfc971a8420e84bde2c7b9
johnd0e <1838643+johnd0e@users.noreply.github.com>
2025-02-13 17:46:57 +0100

    forum.lua: improve *

--- forum.lua
+++ forum.lua
@@ -4,6 +4,9 @@
 function Str (el)
   -- normal way of escaping the asterisk is \*, but that does not work in Litedown.
   if el.text:find"*" then
+    if el.text=="*" then
+      return pandoc.RawInline("markdown","[i]*[/i]") -- prevent parsing as list item
+    end
     local parts,index,a,b = {},1
     repeat
       a,b,index = el.text:match("^([^*]*)%*([^*]*)()",index)
