4ec4df9cb8475d1830e1340f84289657edfd1db0
johnd0e <1838643+johnd0e@users.noreply.github.com>
2025-03-21 23:06:41 +0100

    forum.lua: enhance

--- forum.lua
+++ forum.lua
@@ -1,19 +1,26 @@
 -- phpBB3 uses own markdown parser (https://s9etextformatter.readthedocs.io/Plugins/Litedown/Syntax/),
 -- which differs in some minor details. That's why some fixes needed.
 
+local symbols = "([*><]+)"
 function Str (el)
   -- normal way of escaping the asterisk is \*, but that does not work in Litedown.
-  if el.text:find"*" then
+  -- the same is with angle brackets
+  if el.text:find(symbols) then
     if el.text=="*" then
       return pandoc.RawInline("markdown","[i]*[/i]") -- prevent parsing as list item
     end
-    local parts,index,a,b = {},1
+    local parts,from = {},1
     repeat
-      a,b,index = el.text:match("^([^*]*)%*([^*]*)()",index)
-      table.insert(parts, pandoc.Str(a))
-      table.insert(parts, pandoc.RawInline("markdown","*"))
-      table.insert(parts, pandoc.Str(b))
-    until index>el.text:len()
+      local a,b,after = el.text:match("^(.-)"..symbols.."()",from)
+      if not a then
+        table.insert(parts, pandoc.Str(el.text:sub(from)))
+        break
+      elseif a~="" then
+        table.insert(parts, pandoc.Str(a))
+      end
+      table.insert(parts, pandoc.RawInline("markdown",b))
+      from = after
+    until from>el.text:len()
     return parts
   end
 end
