a52a306da261447a559ea0c409c1538cbe02dd65
johnd0e <1838643+johnd0e@users.noreply.github.com>
2025-04-17 11:02:02 +0200

    addLinks.lua: support block elements

--- addLinks.lua
+++ addLinks.lua
@@ -1,7 +1,12 @@
 -- Add links block
 function Pandoc (doc)
-  if doc.meta.links then
-    table.insert(doc.blocks, pandoc.Para(doc.meta.links))
+  local links = doc.meta.links
+  if links then
+    if links[1].t=="Str" or links[1].t=="Plain" or links[1].t=="Link" then
+      links = {pandoc.Para(links)}
+    end
+    doc.blocks:insert(pandoc.HorizontalRule())
+    doc.blocks:extend(links)
     return doc
   end
 end
