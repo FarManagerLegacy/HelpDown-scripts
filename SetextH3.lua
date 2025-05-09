c9125973a00a1e8a7e362066e0e5ef4176881fef
johnd0e <1838643+johnd0e@users.noreply.github.com>
2025-05-10 00:43:31 +0200

    SetextH3.lua: enhance

--- SetextH3.lua
+++ SetextH3.lua
@@ -2,9 +2,14 @@
 -- Useful for Setext-style H2 headers
 
 --luacheck: globals Header
+local parent
 function Header (el)
-  if el.identifier=="" and el.level==2 then
-    el.level = 3
-    return el
+  if el.identifier=="" then
+    if el.level==2 and parent==2 then
+      el.level = 3
+      return el
+    end
+  else
+    parent = el.level
   end
 end
